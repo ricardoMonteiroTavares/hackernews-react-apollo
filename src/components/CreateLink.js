497a9b69c6a7cda47659d7cd60b70dac0e88ac2b
rmonteiro <rmonteiro@marlin.com.br>
2022-05-01 18:50:36 -0300

    Ajuste no CreateLink

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useMutation, gql } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
 
 const CREATE_LINK_MUTATION = gql`
 mutation PostMutation(
@@ -21,11 +22,14 @@ const CreateLink = () => {
     url: ''
   });
 
+  const navigate = useNavigate();
+
   const [createLink] = useMutation(CREATE_LINK_MUTATION, {
     variables: {
       description: formState.description,
       url: formState.url
-    }
+    },
+    onCompleted: () => navigate('/')
   });
 
   return (
