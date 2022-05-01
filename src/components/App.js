be24a71fe0e796b7bf777b13156296728ee50131
rmonteiro <rmonteiro@marlin.com.br>
2022-05-01 01:25:56 -0300

    Adição do objeto CreateLink

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import './../styles/App.css';
 import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 function App() {
   return (
-    <LinkList />
+    <>
+      <CreateLink />
+      <br/>
+      <LinkList />
+    </>
   );
 }
 
