cfd5981afc2e77d3ea8c63f462fe90df180ca4ff
rmonteiro <rmonteiro@marlin.com.br>
2022-05-01 21:52:49 -0300

    Adição das subscrições

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ import {
   InMemoryCache
 } from '@apollo/client';
 
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
@@ -28,10 +32,34 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
